4b086ad23c0fcde37f033d6bee5e60863afff048
isak <isak@isak.lan>
2024-11-28 14:59:05 +0100

    Final?

--- apt.sh
+++ apt.sh
@@ -7,7 +7,7 @@ sudo ln -sf $ROOT/sources.list /etc/apt/sources.list
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install zsh fzf ripgrep fd-find alacritty can-utils clangd clang cmake curl firefox htop neovim python3-pip tmux
+sudo apt install zsh fzf ripgrep fd-find alacritty can-utils clangd clang cmake curl firefox htop neovim python3-pip tmux plasma-workspace kde-plasma-desktop plasma-desktop -y
 chsh -s $(which zsh)
 ln -sf $ROOT/.zshrc $HOME
 
--- link.sh
+++ link.sh
@@ -9,14 +9,14 @@ ln -sf $ROOT/alacritty $HOME/.config/
 ln -sf $ROOT/nvim $HOME/.config/nvim
 ln -sf $ROOT/tmux-addons $HOME/.local/
 
-#wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Noto.zip
-#rm $HOME/.local/share/fonts
-#mkdir -p $HOME/.local/share/fonts
-#mv Noto.zip $HOME/.local/share/fonts
-#cd $HOME/.local/share/fonts
-#unzip Noto.zip 
-#rm Noto.zip
-#cd -
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Noto.zip
+rm $HOME/.local/share/fonts
+mkdir -p $HOME/.local/share/fonts
+mv Noto.zip $HOME/.local/share/fonts
+cd $HOME/.local/share/fonts
+unzip Noto.zip 
+rm Noto.zip
+cd -
 
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 cd ~/.oh-my-zsh
