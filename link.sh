4688b66b0fd0c8250221de7526fdc2dc45941e6d
isak <isakhansson2000@gmail.com>
2025-06-01 10:10:28 +0200

    update

--- link.sh
+++ link.sh
@@ -10,3 +10,8 @@ ln -sf $ROOT/alacritty $HOME/.config/
 ln -sf $ROOT/nvim $HOME/.config/nvim
 ln -sf $ROOT/tmux-addons $HOME/.local/
 
+ln -sf $ROOT/clangd $HOME/.config/clangd
+ln -sf $ROOT/hypr $HOME/.config/hypr
+ln -sf $ROOT/waybar $HOME/.config/waybar
+ln -sf $ROOT/.vimrc $HOME/.vimrc
+
---
+++ ohmyzsh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ~/.oh-my-zsh/custom/plugins
+
+git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
+
+cd -
